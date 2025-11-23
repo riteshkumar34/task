b731d14fa6b00b0e2b57f20d36bd0941083081af
riteshkumar34 <riteshandshan@gmail.com>
2025-11-23 23:04:01 +0530

    push

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: "https://task46.onrender.com/api",
 });
 
-// Token automatically send karega
+// Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
