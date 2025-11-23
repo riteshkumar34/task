e4e2a9817a467ca43e5e57db2ad1bae1cbec88b9
riteshkumar34 <riteshandshan@gmail.com>
2025-11-23 23:07:35 +0530

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: "https://task46.onrender.com/api",
 });
 
-// Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
