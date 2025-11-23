195948c4f9c27de2c5c04b245742db9247df5805
riteshkumar34 <riteshandshan@gmail.com>
2025-11-23 22:58:21 +0530

    push

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,14 +1,10 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : `${import.meta.env.VITE_API_URL}/api`;
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
+// Token automatically send karega
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
