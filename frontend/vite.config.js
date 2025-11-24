4092136282e52baeb1fc0f875bc65279f9f869d4
riteshkumar34 <riteshandshan@gmail.com>
2025-11-24 10:41:25 +0530

    final update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Tailwind plugin
+  ],
   server: {
     fs: {
-      strict: false
-    }
-  }
+      strict: false,
+    },
+  },
 });
